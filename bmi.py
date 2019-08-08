7f8f48a99bd35163566705ebf6fff7e070080011
Veronika Novotna <veronika.novotna@protonmail.com>
2019-08-08 12:03:25 +0200

    adding a quit function

--- bmi.py
+++ bmi.py
@@ -1,18 +1,20 @@
 print("Welcome to the Basal Metabolic Index calculator!")
 
 height = input("Insert your height in centimetres: ")
-weight = input("Insert your weight in kilograms: ")
-
 try:
     val = int(height)
     height = int(height)
 except ValueError:
     print("Please use numbers to insert height!")
+    quit()
+
+weight = input("Insert your weight in kilograms: ")
 try:
     val = int(weight)
     weight = int(weight)
 except ValueError:
     print("Please use numbers to insert weight!")
+    quit()
 
 if isinstance(height, int) and isinstance(weight, int):
     height = height / 100
